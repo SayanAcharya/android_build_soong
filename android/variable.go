098954cdcc60e21a416b4731a377f58abefd0c2f
Rashed Abdel-Tawab <rashed@linux.com>
2017-09-13 22:26:32 -0400

    soong: Rename our android package to android_aosp
    Change-Id: Id5f9577b538820fcee351d6bea25a9626cf2dc49
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- android/variable.go
+++ android/variable.go
@@ -20,7 +20,7 @@ import (
 	"runtime"
 	"strings"
 
-	"aosp/soong/android"
+	"aosp/soong/android_aosp"
 
 	"github.com/google/blueprint/proptools"
 )
@@ -110,7 +110,7 @@ type variableProperties struct {
 		}
 
 		// include AOSP variables
-		AOSP android.Product_variables
+		AOSP android_aosp.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -194,7 +194,7 @@ type productVariables struct {
 	TargetUsesQCOMBsp *bool `json:",omitempty"`
 
 	// include AOSP variables
-	AOSP android.ProductVariables
+	AOSP android_aosp.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
